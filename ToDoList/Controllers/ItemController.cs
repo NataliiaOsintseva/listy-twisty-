6258fd46ce57831b9f10512bed04685cae46cb0c
Nataliia Osintseva <nataly.osya@gmail.com>
2016-10-21 00:16:41 +0300

    Basic stuff

---
+++ ToDoList/Controllers/ItemController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private ItemDbContext context = new ItemDbContext();
+        
+        // GET: Item
+        public ActionResult Index()
+        {
+            var items = from i in context.Items select i;
+            return View(items);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ID,Title,Description,IsFinished")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Items.Add(item);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsFinished { get; set; }
+
+    }
+
+    public class ItemDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+}
