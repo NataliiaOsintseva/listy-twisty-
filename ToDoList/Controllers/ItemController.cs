66f0f8030a7347be18ab3e414668e41a65b94b19
NataliiaOsintseva <nataly.osya@gmail.com>
2016-10-21 18:14:40 +0300

    More

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ToDoList.Models;
@@ -10,12 +12,12 @@ namespace ToDoList.Controllers
     public class ItemController : Controller
     {
 
-        private ItemDbContext context = new ItemDbContext();
+        private ItemDbContext db = new ItemDbContext();
         
         // GET: Item
         public ActionResult Index()
         {
-            var items = from i in context.Items select i;
+            var items = from i in db.Items select i;
             return View(items);
         }
 
@@ -25,21 +27,68 @@ namespace ToDoList.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID,Title,Description,IsFinished")] Item item)
         {
             if (ModelState.IsValid)
             {
-                context.Items.Add(item);
-                context.SaveChanges();
+                db.Items.Add(item);
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
             return View(item);
         }
 
-        public ActionResult Edit()
+        public ActionResult Edit(int? id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Description,IsFinished")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
     }
